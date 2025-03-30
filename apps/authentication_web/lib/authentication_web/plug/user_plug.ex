da509b793ab409cf69691d1fae443aef49806f38
hlista <hlista91@gmail.com>
2025-03-30 16:18:26 -0500

    Adding a plug to get a users info from a jwt token and put it into the context for absinthe

---
+++ apps/authentication_web/lib/authentication_web/plug/user_plug.ex
@@ -0,0 +1,32 @@
+defmodule AuthenticationWeb.Plug.UserPlug do
+  import Plug.Conn
+
+  @behaviour Plug
+
+  @impl Plug
+  def init(opts), do: opts
+
+  @impl Plug
+  def call(conn, _opts \\ []) do
+    with {:ok, jwt_token} < get_jwt(conn),
+         {:ok, %{
+          "email" => email,
+          "user_id" => user_id
+          }} <-
+            AuthenticationService.Token.verify_and_validate(jwt_token) do
+      Absinthe.Plug.assign_context(conn, %{
+        current_user: %{
+          id: user_id,
+          email: email
+        }
+      })
+    end
+  end
+
+  defp get_jwt(conn) do
+    case get_req_header(conn, "authorization") do
+      ["Bearer " <> jwt_token] -> {:ok, jwt_token}
+      _ -> {:error, "not authorized"}
+    end
+  end
+end
